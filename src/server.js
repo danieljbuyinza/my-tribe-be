a1642df52666f6f1058760ecc76195a654b6614c
Daniel Buyinza <danieljbuyinza@gmail.com>
2025-12-10 01:41:19 +0300

    Add MongoDB to Node.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 const tribes = [
 	{
@@ -28,21 +29,58 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/api/tribes/:slug/suggest", (req, res) => {
+let db;
+
+async function connectToDatabase() {
+	const uri = "mongodb://127.0.0.1:27017";
+	const client = new MongoClient(uri, {
+		serverApi: {
+			version: ServerApiVersion.v1,
+			strict: true,
+			deprecationErrors: true,
+		},
+	});
+	await client.connect();
+	db = client.db("my-tribe-db");
+}
+
+app.get("/api/tribes/:slug", async (req, res) => {
+	const { slug } = req.params;
+	const tribe = await db.collection("tribes").findOne({ slug });
+	res.json({ tribe });
+});
+
+app.post("/api/tribes/:slug/suggest", async (req, res) => {
 	const { slug } = req.params;
 	const { suggestion, suggestedBy } = req.body;
+	const newSuggestion = { suggestion, suggestedBy };
 
-	const tribe = tribes.find((tribe) => tribe.slug === slug);
-	tribe.suggestions.push({ suggestion, suggestedBy });
+	const tribe = await db.collection("tribes").findOneAndUpdate(
+		{ slug },
+		{
+			$push: { suggestions: newSuggestion },
+		},
+		{ returnDocument: "after" }
+	);
 	res.json({ tribe });
 });
 
-app.post("/api/tribes/:slug/upvote", (req, res) => {
-	const tribe = tribes.find((tribe) => tribe.slug === req.params.slug);
-	tribe.upvotes += 1;
+app.post("/api/tribes/:slug/upvote", async (req, res) => {
+	const { slug } = req.params;
+	const tribe = await db.collection("tribes").findOneAndUpdate(
+		{ slug },
+		{
+			$inc: { upvotes: 1 },
+		},
+		{ returnDocument: "after" }
+	);
 	res.json({ tribe });
 });
 
-app.listen(8000, function () {
-	console.log("Listening on port 8000...");
-});
+async function startServer() {
+	await connectToDatabase();
+	app.listen(8000, function () {
+		console.log("Listening on port 8000...");
+	});
+}
+startServer();
